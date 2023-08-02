9734360eea4cf04343f13e404500f646ae54230a
Uriel Solis <euss99@hotmail.com>
2023-08-02 14:19:54 -0600

    Init the project

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<script setup>
+import Presupuesto from "./components/Presupuesto.vue";
+</script>
+
+<template>
+  <div>
+    <header>
+      <h1>Planificador de gastos</h1>
+
+      <div class="contenedor-header contenedor sombra">
+        <Presupuesto />
+      </div>
+    </header>
+  </div>
+</template>
+
+<style>
+:root {
+  --azul: #3b82f6;
+  --azul-hover: #1048a4;
+  --blanco: #fff;
+  --gris-claro: #f5f5f5;
+  --gris: #94a3b8;
+  --gris-oscuro: #64748b;
+  --negro: #000;
+}
+
+html {
+  font-size: 62.5%;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+
+body {
+  font-size: 1.6rem;
+  font-family: "Lato", sans-serif;
+  background-color: var(--gris-claro);
+}
+
+h1 {
+  font-size: 4rem;
+}
+
+h2 {
+  font-size: 3rem;
+}
+
+header {
+  background-color: var(--azul);
+}
+
+header h1 {
+  padding: 3rem 0;
+  margin: 0;
+  color: var(--blanco);
+  text-align: center;
+}
+
+.contenedor {
+  width: 90%;
+  max-width: 80rem;
+  margin: 0 auto;
+}
+
+.contenedor-header {
+  margin-top: -5rem;
+  transform: translateY(5rem);
+  padding: 5rem;
+}
+
+.sombra {
+  box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
+  background-color: var(--blanco);
+  border-radius: 1.2rem;
+  padding: 5rem;
+}
+</style>
---
+++ src/components/Presupuesto.vue
@@ -0,0 +1,74 @@
+<script setup>
+import { ref } from "vue";
+
+const presupuesto = ref(0);
+
+const definirPresupuesto = () => {
+  if (presupuesto.value <= 0) {
+    console.log("Presupuesto no valido");
+  }
+};
+</script>
+
+<template>
+  <form class="presupuesto" @submit.prevent="definirPresupuesto">
+    <div class="campo">
+      <label for="nuevo-presupuesto">Definir presupuesto</label>
+      <input
+        type="number"
+        id="nuevo-presupuesto"
+        class="nuevo-presupuesto"
+        placeholder="Añade tu presupuesto"
+        min="0"
+        v-model.number="presupuesto"
+      />
+    </div>
+
+    <input type="submit" value="Definir presupuesto" />
+  </form>
+</template>
+
+<style scoped>
+.presupuesto {
+  width: 100%;
+}
+
+.campo {
+  display: grid;
+  gap: 2rem;
+}
+
+.presupuesto label {
+  font-size: 2.2rem;
+  color: var(--azul);
+  font-weight: 700;
+}
+
+.presupuesto input[type="number"] {
+  background-color: var(--gris-claro);
+  border-radius: 1rem;
+  padding: 1rem;
+  border: none;
+  font-size: 2.2rem;
+  text-align: center;
+}
+
+.presupuesto input[type="submit"] {
+  background-color: var(--azul);
+  border-radius: 1rem;
+  border: none;
+  padding: 1rem;
+  text-align: center;
+  font-size: 2rem;
+  margin-top: 2rem;
+  color: var(--blanco);
+  font-weight: 900;
+  width: 100%;
+  transition: background-color 300ms ease;
+}
+
+.presupuesto input[type="submit"]:hover {
+  background-color: var(--azul-hover);
+  cursor: pointer;
+}
+</style>
